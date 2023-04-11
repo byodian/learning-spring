eb55ea6fbf9d94f19619b79773bc55ee42cf0b12
byodian <byj6696@gmail.com>
2023-04-11 16:06:55 +0800

    multi-module

---
+++ multi-module/library/src/main/java/com/example/multimodule/service/MyService.java
@@ -0,0 +1,17 @@
+package com.example.multimodule.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@Service
+@EnableConfigurationProperties(ServiceProperties.class)
+public class MyService {
+    private final ServiceProperties serviceProperties;
+    public MyService(ServiceProperties serviceProperties) {
+        this.serviceProperties = serviceProperties;
+    }
+
+    public String message () {
+        return this.serviceProperties.getMessage();
+    }
+}
