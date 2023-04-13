91890fd11352573bec6a2a42c3280c1221122cd9
byodian <byj6696@gmail.com>
2023-04-13 17:45:19 +0800

    Accessing data with MySQL

---
+++ springboot/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Greeting from spring boot!";
+    }
+}
